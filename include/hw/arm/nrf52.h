7a330f9c8a885197bbe47d0edbe9bcb2714c52f2
catmaster <catmaster1010@protonmail.com>
2026-09-03 15:46:26 -0400

    Added NRF52 NVMC UICR, and flash block write tracking and other stuffs

--- include/hw/arm/nrf52.h
+++ include/hw/arm/nrf52.h
@@ -10,11 +10,14 @@
 #define NRF52_FLASH_BASE      0x00000000
 #define NRF52_FICR_BASE       0x10000000
 #define NRF52_UICR_BASE       0x10001000
+
+
 #define NRF52_SRAM_BASE       0x20000000
 
 #define NRF52_NVMC_BASE       0x4001E000
 
 #define NRF52_PERIPHERAL_SIZE 0x00001000
+
 /*
 #define NRF51_IOMEM_BASE      0x40000000
 #define NRF51_IOMEM_SIZE      0x20000000
@@ -29,6 +32,7 @@
 #define NRF51_PRIVATE_SIZE    0x10000000
 */
 #define NRF52_PAGE_SIZE       0x1000
+#define NRF52_BLOCK_SIZE      0x200
 
 
 #endif
--- include/hw/nvram/nrf52_nvm.h
+++ include/hw/nvram/nrf52_nvm.h
@@ -1,6 +1,7 @@
 #ifndef NRF52_NVM_H
 #define NRF52_NVM_H
 
+#include "glib.h"
 #include "qemu/osdep.h"
 #include "hw/core/sysbus.h"
 #include "qom/object.h"
@@ -11,25 +12,42 @@ OBJECT_DECLARE_SIMPLE_TYPE(NRF52NVMState, NRF52_NVM)
 
 #define NRF52_NVMC_SIZE         0x1000
 
+/* READY */
 #define NRF52_NVMC_READY        0x400
 #define NRF52_NVMC_READY_READY  0x01
 
-
-#define NRF52_NVMC_CONFIG       0x504
+/* CONFIG */
+#define NRF52_NVMC_CONFIG       0x504 
 #define NRF52_NVMC_CONFIG_MASK  0x03
 #define NRF52_NVMC_CONFIG_REN   0x0
 #define NRF52_NVMC_CONFIG_WEN   0x01
 #define NRF52_NVMC_CONFIG_EEN   0x02
 
-#define NRF52_NVMC_ERASEPCR1    0x508
-#define NRF52_NVMC_ERASEPCR0    0x510
-#define NRF52_NVMC_ERASEALL     0x50C
+/* ERASEPAGE */
+#define NRF52_NVMC_ERASEPAGE    0x508 //  ERASEPCR1 is deprecated
+
+/* ERASEALL */
+#define NRF52_NVMC_ERASEALL     0x50C 
+#define NRF52_NVMC_ERASEALL_ERASE    0x1
+
+/* ERASEUICR */
 #define NRF52_NVMC_ERASEUICR    0x514
-#define NRF52_NVMC_ERASE        0x01
+#define NRF52_NVMC_ERASEUICR_ERASE  0x1
+
+/* ICACHECNF */
+#define NRF52_NVMC_ICACHECNF    0x540
+#define NRF52_NVMC_ICACHECNF_CHACHEEN_ENABLE    0x1
+#define NRF52_NVMC_ICACHECNF_CACHEPROFEN_ENABLE 0x100
+
+/* IHIT */
+#define NRF52_NVMC_IHIT         0x548
+
+/* IMISS */
+#define NRF52_NVMC_IMISS        0x54C
 
 #define NRF52_UICR_SIZE         0x210
 
-#define NVMC_N_WRITE 181
+#define NRF52_NVMC_N_WRITE 181
 
 struct NRF52NVMState {
     SysBusDevice parent_obj;
@@ -44,6 +62,9 @@ struct NRF52NVMState {
     uint8_t *storage;
 
     uint32_t config;
+    uint32_t icachecnf;
+
+    GHashTable *num_flash_writes_hash;
 
 };
 
